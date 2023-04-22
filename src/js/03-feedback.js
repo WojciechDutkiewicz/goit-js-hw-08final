f32b914f6f90d31ffca80224690e27a3205c4cdb
Wojciech Dutkiewicz <dutwoj95@gmail.com>
2023-04-22 13:19:17 +0200

    ok

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const feebackForm = document.querySelector('.feedback-form');
+let formInputValue = {};
+const submitBTN = 'feedback-form-state';
+
+feebackForm.addEventListener('submit', submitFeedbacForm);
+
+function storageInputValue(ev) {
+  formInputValue[ev.target.name] = ev.target.value;
+  const inputValue = JSON.stringify(formInputValue);
+  localStorage.setItem(submitBTN, inputValue);
+}
+
+function submitFeedbacForm(ev) {
+  ev.preventDefault();
+  ev.currentTarget.reset();
+  localStorage.removeItem(submitBTN);
+  console.log(formInputValue);
+}
+
+function presentStorageValue() {
+  const StorageValue = localStorage.getItem(submitBTN);
+  if (StorageValue) {
+    formInputValue = JSON.parse(StorageValue);
+    feebackForm.email.value = formInputValue.email ? formInputValue.email : '';
+    feebackForm.message.value = formInputValue.message
+      ? formInputValue.message
+      : '';
+  }
+}
+
+presentStorageValue();
+
+feebackForm.addEventListener('input', throttle(storageInputValue, 500));
